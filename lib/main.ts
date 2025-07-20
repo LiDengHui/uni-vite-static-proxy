e30965583c706a9816b35d9485abaa4e58599c9c
denghui.li <3379489032@qq.com>
2025-07-20 12:22:24 +0800

    build: change build style

--- lib/main.ts
+++ lib/main.ts
@@ -1,105 +1,115 @@
-import {createProxyMiddleware} from 'http-proxy-middleware';
-import express from 'express';
-import http from "http";
-import {Plugin} from "vite";
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import express from 'express'
+import http from 'http'
+import { Plugin } from 'vite'
 import fs from 'fs'
-import path from 'path';
+import path from 'path'
 
-const file = path.join(process.cwd(), "_catch.json");
+const file = path.join(process.cwd(), '_catch.json')
 // 设置
 const setCache = (key: string, value: string) => {
-    let cache = fs.existsSync(file) ? JSON.parse(fs.readFileSync(file).toString()) : {};
-    cache[key] = value;
-    fs.writeFileSync(file, JSON.stringify(cache, null, 2));
-};
+  let cache = fs.existsSync(file)
+    ? JSON.parse(fs.readFileSync(file).toString())
+    : {}
+  cache[key] = value
+  fs.writeFileSync(file, JSON.stringify(cache, null, 2))
+}
 
 // c
 const getCache = (key?: string) => {
-    if (!fs.existsSync(file)) return null;
-    const cache = JSON.parse(fs.readFileSync(file).toString());
-    return key ? (cache[key] || null) : cache;
-};
+  if (!fs.existsSync(file)) return null
+  const cache = JSON.parse(fs.readFileSync(file).toString())
+  return key ? cache[key] || null : cache
+}
 const getLastKey = () => {
-    if (!fs.existsSync(file)) return null;
-    const cache = JSON.parse(fs.readFileSync(file, "utf-8"));
+  if (!fs.existsSync(file)) return null
+  const cache = JSON.parse(fs.readFileSync(file, 'utf-8'))
 
-    const keys = Object.keys(cache);
-    return keys.length ? keys[keys.length - 1] : null;
-};
+  const keys = Object.keys(cache)
+  return keys.length ? keys[keys.length - 1] : null
+}
 const removeKey = (key: string) => {
-    if (!fs.existsSync(file)) return false;
+  if (!fs.existsSync(file)) return false
 
-    const cache = JSON.parse(fs.readFileSync(file, "utf-8"));
+  const cache = JSON.parse(fs.readFileSync(file, 'utf-8'))
 
-    if (!(key in cache)) return false; // key 不存在，返回 false
+  if (!(key in cache)) return false // key 不存在，返回 false
 
-    delete cache[key]; // 删除 key
+  delete cache[key] // 删除 key
 
-    fs.writeFileSync(file, JSON.stringify(cache, null, 2), "utf-8"); // 重新写入文件
+  fs.writeFileSync(file, JSON.stringify(cache, null, 2), 'utf-8') // 重新写入文件
 
-    return true; // 删除成功
-};
-export default function uniViteStaticProxy({port = 3000, staticPath = '', proxyConfigs = []}): Plugin {
-    let app = express();
-    let server: http.Server;
-    return {
-        name: 'uni-vite-static-proxy',
-        async configResolved(): Promise<void> {
-            console.log("123")
-            const lastKey = getLastKey();
+  return true // 删除成功
+}
 
-            if (lastKey && getCache(lastKey) === "open") {
-                return;
-            }
+export interface ProxyConfig {
+  pathFilter: string
+  target: string
+  changeOrigin?: boolean
+  pathRewrite?: Record<string, string>
+}
 
-            // // 静态资源托管
-            if (staticPath) {
-                app.use(express.static(staticPath));
-            }
+export default function uniViteStaticProxy({
+  port = 3000,
+  staticPath = '',
+  proxyConfigs = [] as ProxyConfig[],
+}): Plugin {
+  let app = express()
+  let server: http.Server
+  return {
+    name: 'uni-vite-static-proxy',
+    async configResolved(): Promise<void> {
+      const lastKey = getLastKey()
 
-            // 配置多个代理
-            proxyConfigs.forEach(({pathFilter, target, pathRewrite}) => {
-                app.use(
-                    pathFilter,
-                    createProxyMiddleware({
-                        target,
-                        changeOrigin: true,
-                        pathRewrite: pathRewrite || {},
-                    })
-                );
-            });
+      if (lastKey && getCache(lastKey) === 'open') {
+        return
+      }
 
-            const key = Date.now().toString();
-            try {
-                // 监听端口
-                server = app.listen(port, () => {
+      // // 静态资源托管
+      if (staticPath) {
+        app.use(express.static(staticPath))
+      }
 
-                    setCache(key, "open")
-                    console.log(`Express server running on port ${port}`);
-                });
-            } catch (e) {
-                console.log(e);
-            }
+      // 配置多个代理
+      proxyConfigs.forEach(({ pathFilter, target, pathRewrite }) => {
+        app.use(
+          pathFilter,
+          createProxyMiddleware({
+            target,
+            changeOrigin: true,
+            pathRewrite: pathRewrite || {},
+          })
+        )
+      })
 
-            server.on('close', () => {
-                console.log('Express server close ');
-                removeKey(key);
-            })
+      const key = Date.now().toString()
+      try {
+        // 监听端口
+        server = app.listen(port, () => {
+          setCache(key, 'open')
+          console.log(`Express server running on port ${port}`)
+        })
+      } catch (e) {
+        console.log(e)
+      }
 
-            server.on('error', (x: any) => {
-                console.log(x)
-            })
+      server.on('close', () => {
+        console.log('Express server close ')
+        removeKey(key)
+      })
 
-            process.on('SIGINT', () => {
-                if (server) {
-                    server.close();
-                    removeKey(key);
-                    process.exit();
-                }
-            });
-            return;
-        },
+      server.on('error', (x: any) => {
+        console.log(x)
+      })
 
-
-    };
+      process.on('SIGINT', () => {
+        if (server) {
+          server.close()
+          removeKey(key)
+          process.exit()
+        }
+      })
+      return
+    },
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: './lib/main.ts',
-            name: 'index',
-            fileName: 'index',
-        },
-        target: 'node14',
-        rollupOptions: {
-            external: ['express', 'http-proxy-middleware','fs', 'path'], // 指定不打包的模块
-            output: {
-                globals: {
-                    'http-proxy-middleware': 'httpProxyMiddleware',
-                    'express': 'express'
-                }
-            }
-        },
+  build: {
+    ssr: true,
+    lib: {
+      entry: './lib/main.ts',
+      name: 'index',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
     },
-    plugins: [dts()],
+    rollupOptions: {
+      external: ['express', 'http-proxy-middleware', 'fs', 'path'], // 指定不打包的模块
+      output: {
+        globals: {
+          'http-proxy-middleware': 'httpProxyMiddleware',
+          express: 'express',
+          fs: 'fs',
+          path: 'path',
+        },
+      },
+    },
+  },
+  plugins: [dts()],
 })
